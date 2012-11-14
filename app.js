3f37859a90131855444863cf77e08cbf0629cb02
Carsten Ringe <carsten@kopis.de>
2012-11-14 11:21:31 +0100

    Save and load pastes!
    Finally, saving and loading pastes is working. Now it's ready to be
    deployed again.

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 var connect = require('connect')
+   ,render = require('connect-render')
    ,urlrouter = require('urlrouter')
-   ,redis   = require('redis-url').connect(process.env.REDISTOGO_URL || "http://127.0.0.1:6379")
+   ,redis = require('redis-url').connect(process.env.REDISTOGO_URL || "http://127.0.0.1:6379")
 ;
 
 var port = process.env.PORT || 8888;
 
-var app = connect()
+var app = connect(render({
+		root: __dirname + '/views',
+		layout: 'index.html',
+		cache: false
+	}))
 	.use(connect.static('public'))
 	.use(connect.favicon())
 	.use(connect.bodyParser())
@@ -15,20 +20,26 @@ var app = connect()
 			redis.incr('next.id');
 			redis.set(id + ':source', req.body.source);
 			redis.set(id + ':brush', req.body.brush);
-			res.end('<a href="/' + id + '">click here to continue</a>');
+			res.end('<a href="/paste/' + id + '">click here to continue</a>');
 		});
 	});
-	app.get('/:id', function(req, res, next) {
-	var id = req.params.id;
-	redis.get(id + ':source', function(err, source) {
-		redis.get(id + ':brush', function(err, brush) {
-			console.log("source=" + source);
-			console.log("brush=" + brush);
-			res.end(source);
+	app.get('/paste/:id', function(req, res, next) {
+		var id = req.params.id;
+		redis.get(id + ':source', function(err, source) {
+			if(err) {
+				//TODO display message if paste not found
+				res.render('index.html', {msg: 'Not found!'});
+			} else {
+				redis.get(id + ':brush', function(err, brush) {
+					res.render('index.html', {source: source, brush: brush});
+				});
+			}
 		});
 	});
-  });
-}))
+	}))
+	.use(function(req, res) {
+		res.render('index.html', {source: '', brush: '', url: req.url });
+	})
 	.listen(port);
 console.log('Server running at http://127.0.0.1:' + port);
 

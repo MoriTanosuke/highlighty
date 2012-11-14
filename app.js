3f43a2af47e44606c4f450cde8e6478250073963
Carsten Ringe <carsten@kopis.de>
2012-11-14 08:40:38 +0100

    Prepare for redis.

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
-var http = require('http')
-   ,url  = require('url')
-   ,fs   = require('fs');
+var http   = require('http')
+   ,url    = require('url')
+   ,querystring = require('querystring')
+   //,redis  = require('redis').connect(process.env.REDISTOGO_URL)
+   ,fs     = require('fs');
+
+
 var port = process.env.PORT || 8888;
 http.createServer(function (request, response) {
   var f = ".";
-  if(request.url == "/") {
+  if((request.url == "/" || request.url == "/index.html")
+	 && request.method == "GET") {
+    f += "/index.html";
+  } else if((request.url == "/" || request.url == "/index.html")
+	 && request.method == "POST") {
+    var body = '';
+    request.on('data', function(chunk) {
+      body += chunk.toString();
+    });
+    request.on('end', function() {
+      var decodedBody = querystring.parse(body);
+      console.log('source=' + decodedBody['source']);
+      console.log('brush=' + decodedBody['brush']);
+      //TODO write to redis
+    });
     f += "/index.html";
   } else {
     f += url.parse(request.url)['pathname'];
@@ -22,8 +40,8 @@ http.createServer(function (request, response) {
     }
     response.writeHead(200, header);
     return response.end(data);
-  });
+});
 }).listen(port);
 
-console.log('Server running at http://127.0.0.1:8124/');
+console.log('Server running at http://127.0.0.1:' + port);
 

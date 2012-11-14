65b2ab8e64cb391156537dc49dac9ad5646f820b
Carsten Ringe <carsten@kopis.de>
2012-11-14 09:59:24 +0100

    Prepare for REDIS
    I need to figure out how to handle requests to specific URLs only. I
    want to receive the POSTs, but leave all static file handling to
    connect. And I need to load stuff from REDIS and populate the fields in
    index.html when a request goes to "/123" or the like with "123" being
    the REDIS key for a previously saved paste.

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 var connect = require('connect')
-   //,redis  = require('redis').connect(process.env.REDISTOGO_URL)
+   ,redis   = require('redis-url').connect(process.env.REDISTOGO_URL || "http://127.0.0.1:6379")
 ;
 
 var port = process.env.PORT || 8888;
 
 var app = connect()
-	.use(connect.logger())
 	.use(connect.static('public'))
+	.use(connect.favicon())
+	.use(connect.bodyParser())
+	.use(function(req, res) {
+		redis.get('next.id', function(err, id) {
+			redis.incr('next.id');
+			redis.set(id + ':source', req.body.source);
+			redis.set(id + ':brush', req.body.brush);
+			res.end('<a href="/' + id + '">click here to continue</a>');
+		});
+	})
 	.listen(port);
 console.log('Server running at http://127.0.0.1:' + port);
 

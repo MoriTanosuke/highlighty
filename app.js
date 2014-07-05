ee42377c299b5137574ab8156b44a364610f3f67
Carsten <carsten@kopis.de>
2014-07-05 19:47:57 +0200

    Prepare to delete old highlights

--- app.js
+++ app.js
@@ -15,6 +15,17 @@ var app = connect(render({
 	.use(connect.favicon())
 	.use(connect.bodyParser())
 	.use(urlrouter(function(app) {
+	// clean up old entries from Redis
+	app.get('/cleanup', function(req, res, next) {
+		// remove all entries, only keep 10
+		var len = redis.llen('used.ids');
+		for(int i = len; i >= 0; i--) {
+		    console.log("var id = redis.lpop('used.ids')");
+			console.log("redis.del(id + ':source')");
+			console.log("redis.del(id + ':brush')");
+		}
+		res.render('index.html', {msg: 'Highlights cleaned up.'});
+	});
 	app.post('/save', function(req, res, next) {
 		redis.get('next.id', function(err, id) {
 			if(err) {
